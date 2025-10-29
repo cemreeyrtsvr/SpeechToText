5a8c7d3f60e3adef75f116cef2af9478f6aff6f7
Cemre Yurtsever <cyurtsever64@gmail.com>
2025-10-29 21:47:14 +0300

    Proje dosyası ekle.

---
+++ SpeechToText/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace SpeechToText
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btnStart = new Button();
+            btnStop = new Button();
+            txtTranscript = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // btnStart
+            // 
+            btnStart.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnStart.Location = new Point(157, 312);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(94, 29);
+            btnStart.TabIndex = 0;
+            btnStart.Text = "\"Başlat\"";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // btnStop
+            // 
+            btnStop.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnStop.Location = new Point(770, 312);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(94, 29);
+            btnStop.TabIndex = 1;
+            btnStop.Text = "\"Bitir\"\r\n";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // txtTranscript
+            // 
+            txtTranscript.Font = new Font("Yu Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtTranscript.Location = new Point(157, 511);
+            txtTranscript.Multiline = true;
+            txtTranscript.Name = "txtTranscript";
+            txtTranscript.ScrollBars = ScrollBars.Vertical;
+            txtTranscript.Size = new Size(707, 166);
+            txtTranscript.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Yu Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label1.ForeColor = Color.Transparent;
+            label1.Location = new Point(171, 202);
+            label1.Name = "label1";
+            label1.Size = new Size(693, 26);
+            label1.TabIndex = 3;
+            label1.Text = "---Lütfen Başlat Tuşuna Basarak Çevirmek İstediğiniz Metni Söyleyiniz---";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 27F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.RosyBrown;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1007, 748);
+            Controls.Add(label1);
+            Controls.Add(txtTranscript);
+            Controls.Add(btnStop);
+            Controls.Add(btnStart);
+            Font = new Font("Viner Hand ITC", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = Color.RosyBrown;
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Çevirici";
+            Click += btnStop_Click;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnStart;
+        private Button btnStop;
+        private TextBox txtTranscript;
+        private Label label1;
+    }
+}
---
+++ SpeechToText/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using NAudio.Wave;
+using Vosk;
+using Newtonsoft.Json.Linq;
+
+namespace SpeechToText
+{
+    public partial class Form1 : Form
+    {
+        private WaveInEvent wave;
+        private VoskRecognizer recognizer;
+        private Model model;
+        private bool kayıt = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnStop.Enabled = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            string modelPath = Path.Combine(Application.StartupPath, "model/vosk-model-small-tr-0.3");
+            if (!Directory.Exists(modelPath))
+            {
+                
+                MessageBox.Show("Model klasörü bulunamadı!\n" + modelPath);
+                return;
+            }
+
+            Vosk.Vosk.SetLogLevel(-1);
+            model = new Model(modelPath);
+            recognizer = new VoskRecognizer(model, 16000.0f);
+
+            wave = new WaveInEvent
+            {
+                WaveFormat = new WaveFormat(16000, 1)
+            };
+            wave.DataAvailable += WaveIn_DataAvailable;
+
+            wave.StartRecording();
+            kayıt = true;
+
+            txtTranscript.AppendText(" Dinleniyor... /r/n");
+
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (recognizer.AcceptWaveform(e.Buffer, e.BytesRecorded))
+            {
+                var result = JObject.Parse(recognizer.Result());
+                var text = result["text"]?.ToString();
+
+                if (!string.IsNullOrEmpty(text))
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        txtTranscript.AppendText(text + " ");
+                    }));
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (kayıt)
+            {
+                kayıt = false;
+                wave.StopRecording();
+
+                var finalResult = JObject.Parse(recognizer.FinalResult());
+                var finalText = finalResult["text"]?.ToString();
+                if (!string.IsNullOrEmpty(finalText))
+                {
+                    txtTranscript.AppendText(finalText + "\r\n");
+                }
+
+                wave.Dispose();
+                recognizer.Dispose();
+                model.Dispose();
+                wave = null;
+                recognizer = null;
+                model = null;
+
+              
+                txtTranscript.AppendText("Kayıt durduruldu.");
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+            }
+        }
+    }
+}
